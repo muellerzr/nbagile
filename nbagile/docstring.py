c53ae9776a80a398eaea13cf8eb5277796ffcfc0
Zachary Mueller <muellerzr@gmail.com>
2021-09-17 15:03:01 -0500

    Refactor functions

--- nbagile/docstring.py
+++ nbagile/docstring.py
@@ -36,35 +36,36 @@ def _get_leading(o):
     return len(o) - len(o.lstrip(o[0])), o[0]
 
 # Cell
+from fastcore.xtras import is_listy
+
+# Cell
 def reformat_function(
     source:str, # Source code
 ):
     "Takes messy source code and refactors it into a readable PEP-8 standard style"
+    # Read in docments, parse source code, generate annotations
     docs = dments.docments(source)
     parsed_source = ast.parse(source)
     annos = get_annotations(source)
-    for i in range(len(parsed_source.body[0].args.args)):
-        parsed_source.body[0].args.args[i].annotation = None
+
+    # Set all arg annotations to None
+    _ = apply(lambda x: setattr(x, 'annotation', None), parsed_source.body[0].args.args)
+
     parsed_source.body[0].returns = None
+    body = parsed_source.body[0].body
     unparsed_source = astunparse.unparse(parsed_source).lstrip('\n').split('\n')
-    has_decorator = False
-    if unparsed_source[0].startswith('@'):
-        has_decorator = True
-    if has_decorator:
-        function_definition = '\n'.join(unparsed_source[:2])
-    else:
-        function_definition = unparsed_source[0]
-    # Check if we have a docstring
-    if isinstance(parsed_source.body[0].body[0].value, ast.Str):
-        if has_decorator:
-            function_innards = "\n".join(unparsed_source[3:])
-        else:
-            function_innards = "\n".join(unparsed_source[2:])
-    else:
-        if has_decorator:
-            function_innards = "\n".join(unparsed_source[2:])
-        else:
-            function_innards = "\n".join(unparsed_source[1:])
+    has_decorator = len(parsed_source.body[0].decorator_list) > 0
+
+    # Extract function definition
+    function_definition = '\n'.join(unparsed_source[:2]) if has_decorator else unparsed_source[0]
+
+    # Check if we have a docstring and extract function innards
+    def _extract_innards(is_str:bool):
+        i = 2 if is_str else 1
+        return '\n'.join(unparsed_source[i+1:]) if has_decorator else '\n'.join(unparsed_source[i:])
+
+    function_innards = _extract_innards(isinstance(body[0].value, ast.Str))
+
     def _get_whitespace(): return whitespace_char*num_whitespace
 
     if unparsed_source[2] != '':
@@ -76,40 +77,36 @@ def reformat_function(
             num_whitespace, whitespace_char = _get_leading(unparsed_source[3])
 
     docstring = f'\n{_get_whitespace()}"""'
-    if isinstance(parsed_source.body[0].body[0].value, ast.Str):
+
+    if isinstance(body[0].value, ast.Str):
         _quotes = ("'", '"')
-        orig_docstring = astunparse.unparse(parsed_source.body[0].body[0]).lstrip(whitespace_char).replace(_quotes[0],'').replace(_quotes[1],'')
+        orig_docstring = astunparse.unparse(body[0]).lstrip(whitespace_char).replace(_quotes[0],'').replace(_quotes[1],'')
         orig_docstring = orig_docstring.split('\\n')
-        # Check if this logic can be refactored
-        for i,line in enumerate(orig_docstring):
-            if len(line.strip()) > 0:
-                if len(line.lstrip()) < len(line):
-                    diff = len(line) - len(line.lstrip())
-                    if i == 0:
-                        docstring += f'{line.lstrip()}'
-                    else:
-                        docstring += f'\n{whitespace_char * (diff)}{line.lstrip()}'
-                else:
-                    if i == 0:
-                        docstring += f'{line.lstrip()}'
-                    else:
-                        docstring += f'\n{_get_whitespace()}{line.lstrip()}'
+        def _inner(line, whitespace_char):
+            diff = len(line) - len(line.lstrip())
+            whitespace = whitespace_char * diff if diff > 0 else _get_whitespace()
+            return f'\n{whitespace}{line}'
+
+        o = apply(_inner, orig_docstring, whitespace_char=whitespace_char)
+        o[0] = orig_docstring[0].lstrip()
+        docstring +=  '\n'.join(o)
+
+    param_string = f'\n{_get_whitespace()}Parameters\n{_get_whitespace()}----------\n'
     if len(docs.keys()) >= 1:
-        if len(docs.keys()) >= 1:
-            param_string = f'\n{_get_whitespace()}Parameters\n'
-            param_string += f'{_get_whitespace()}----------\n'
-            for i, param in enumerate(docs.keys()):
-                if param != "return" and param != "self" and param != "cls":
-                    param_string += f'{_get_whitespace()}{param}'
-                    if annos[0][i] is not None:
-                        param_string += f' : {annos[0][i]}'
-                    else:
-                        param_string += f' : any'
-                    param_string += '\n'
-                    if docs[param] is not None:
-                        param_string += f'{whitespace_char * (num_whitespace+2)}{docs[param]}\n'
-        if param_string != f'\n{_get_whitespace()}Parameters\n{_get_whitespace()}----------\n':
-            docstring += param_string
+        param_string = f'\n{_get_whitespace()}Parameters\n{_get_whitespace()}----------\n'
+        for i, param in enumerate(docs.keys()):
+            if param != "return" and param != "self" and param != "cls":
+                param_string += f'{_get_whitespace()}{param}'
+                if annos[0][i] is not None:
+                    param_string += f' : {annos[0][i]}'
+                else:
+                    param_string += f' : any'
+                param_string += '\n'
+                if docs[param] is not None:
+                    param_string += f'{whitespace_char * (num_whitespace+2)}{docs[param]}\n'
+    if param_string != f'\n{_get_whitespace()}Parameters\n{_get_whitespace()}----------\n':
+        docstring += param_string
+
     if (annos[-1] != inspect._empty) and ('return' in docs.keys()):
         docstring += f'\n{_get_whitespace()}Returns\n'
         docstring += f'{_get_whitespace()}-------\n'
